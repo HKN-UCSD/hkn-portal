6c1bdf19353793372b3c6d444b1500dbc5a892fe
Ryan <ryology231@gmail.com>
2023-08-09 23:52:08 -0700

    Removed __pycache__ dirs

--- myapp/spa/views.py
+++
@@ -1,5 +0,0 @@
-from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
-
-class SpaView(LoginRequiredMixin, TemplateView):
-    template_name = "spa/index.html"
