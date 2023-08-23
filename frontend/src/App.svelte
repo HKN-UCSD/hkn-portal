d64ffd8707d25f59f8bee9a07db9857ce1afb275
Roger Lin <kel030@ucsd.edu>
2023-08-23 10:36:43 +0800

    CSS updates

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,8 +1,7 @@
 <script>
     import { onMount } from "svelte";
-	import Sidebar from './Sidebar.svelte';
-	import Navbar from './Navbar.svelte';
-    export let name;
+    import Navbar from "./Components/Navbar.svelte";
+    import Sidebar from "./Components/Sidebar.svelte";
     let apimessage = "Waiting for server...";
 	let showSidebar = false;
     onMount(async () => {
@@ -12,33 +11,9 @@
     });
 </script>
 
-<Navbar bind:showSidebar={showSidebar} toggleSidebar={() => showSidebar = !showSidebar} />
-
-{#if showSidebar}
-  <Sidebar />
-{/if}
-
-<main>
-</main>
+<Navbar />
+<Sidebar />
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ frontend/src/Components/Navbar.svelte
@@ -0,0 +1,36 @@
+<script>
+  
+</script>
+
+<style>
+  .navbar {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 10px 20px;
+    background-color: #4350AF;
+  }
+
+  .navbar-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+
+
+.portal-title {
+  font-size: 24px; /* Adjust as necessary */
+  font-weight: bold;
+  margin-right: 100px; /* Adjust as necessary */
+  color: white;
+}
+
+</style>
+
+<div class="navbar">
+  <div class="navbar-content">
+    <span class="portal-title">HKN Portal</span>
+  </div>
+</div>
+
