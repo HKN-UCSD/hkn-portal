0e55d231607d864ce057defa283c9f1688e0d2c6
ryanyychen <ryan_yychen@hotmail.com>
2023-09-03 11:11:50 +0800

    Use email instead of username

--- myapp/api/forms.py
+++ myapp/api/forms.py
@@ -4,11 +4,12 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class LoginForm(forms.Form):
-   username = forms.CharField(max_length=65)
+   email = forms.CharField(max_length=255)
    password = forms.CharField(max_length=65, widget=forms.PasswordInput)
 
 class RegisterForm(UserCreationForm):
    class Meta:
       model = CustomUser
-      fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+      fields = ['first_name', 'last_name', 'preferred_name', 'email', 'password1', 'password2']
       exclude = ['user_id']
+   first_name = forms.CharField(label='First Name*')
