5f6eeb2c8201171010c7650145169c65f1298385
ryan-s-lee <93450481+ryan-s-lee@users.noreply.github.com>
2023-09-09 20:48:14 -0700

    3 format login page (#5)
    ryan-s-lee & ryanyychen
    
    - Formatted all login/registration related pages
    - Implemented functional login/registration/password reset pages
    - Using email instead of username to login
    - Need to implement stay logged in

--- myapp/spa/views.py
+++ myapp/spa/views.py
@@ -3,11 +3,5 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 
-
 class SpaView(LoginRequiredMixin, TemplateView):
     template_name = "spa/index.html"
-
-
-def logout_view(request):
-    logout(request)
-    return redirect("/accounts/login")
