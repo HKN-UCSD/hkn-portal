7f97d477fd74eb0bc3f50d26d6b0929309b2f748
Ryan Lee <rlee663818@gmail.com>
2023-09-13 09:28:40 -0700

    Retooled REST-independent dev server

--- frontend/rollup.isolation.config.js
+++ frontend/rollup.isolation.config.js
@@ -36,7 +36,7 @@ export default [
 			sourcemap: true,
 			format: 'iife',
 			name: 'app',
-			file: 'public/bundle.js'
+			file: '../myapp/static/bundle.js'
 		},
 		plugins: [
 			svelte({
@@ -68,7 +68,7 @@ export default [
 
 			// Watch the `public` directory and refresh the
 			// browser on changes when not in production
-			!production && livereload('public'),
+			!production && livereload('../myapp/static/frontend'),
 
 			// If we're building for production (npm run build
 			// instead of npm run dev), minify
