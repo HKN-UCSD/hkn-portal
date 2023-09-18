99ffe6728b760b4e23288d8f600c0fe6a6e84e22
Keng Lien(Roger) Lin <98373624+magikarp620@users.noreply.github.com>
2023-09-19 06:58:40 +0800

    1 create navbar for portal (#13)
    * Added navbar and sidebar with button that open and close sidebar
    
    * Upload missing navbar and sidebar files
    
    * CSS updates
    
    * Added different pages and enable sidebar to navigate to them
    
    * modify git ignore file
    
    * Remove unwanted files
    
    * modified gitignore file
    
    * Modified root gitignore
    
    * Added CSS component for the buttons, turned the HKN Portal wording to a home button
    
    * Installed svelte-routing
    
    For some reason svelte-routing was missing from package.json, so trying
    to build the svelte app resulted in a blank screen. Added it back and
    things work on my end.
    
    * Commit before rebasing with master
    
    * Attempt to fix merge conflict
    
    * Import repath function to urls.py
    
    ---------
    
    Co-authored-by: 林耕廉 <keng@lingenglians-MacBook-Air.local>
    Co-authored-by: Ryan <ryology231@gmail.com>

---
+++ frontend/src/Components/Sidebar.svelte
@@ -0,0 +1,42 @@
+<script>
+  let logo = "/static/frontend/HKN-Logo-New-Blue.png";
+</script>
+
+<style>
+  .sidebar {
+    width: 200px;
+    height: 100vh;
+    background-color: #333;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px 0;
+  }
+
+  .sidebar img {
+    width: 150px;
+    margin-bottom: 20px;
+  }
+
+  .sidebar a {
+    color: white;
+    text-decoration: none;
+    margin: 10px 0;
+    padding: 5px 10px;
+    display: block;
+    width: 100%;
+    text-align: center;
+  }
+
+  .sidebar a:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+
+</style>
+
+<div class="sidebar">
+  <img src={logo} alt="HKN logo" />
+  <a href="/home">HomePage</a>
+  <a href="/profile">Profile</a>
+  <a href="/Logout">Logout</a>
+</div>
--- myapp/spa/views.py
+++ myapp/spa/views.py
@@ -1,7 +1,11 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 
 class SpaView(LoginRequiredMixin, TemplateView):
     template_name = "spa/index.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'spa/index.html')
