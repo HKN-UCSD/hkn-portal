1e06d4aa6a086e1bf8051b0cbeaafbbb470026e3
Roger Lin <kel030@ucsd.edu>
2023-09-20 09:04:08 +0800

    Basic model of the calendar, (will need style, api call to events afterward)

---
+++ frontend/src/Pages/EventCalendar.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { onMount } from 'svelte';
+    import { Calendar } from '@fullcalendar/core';
+    import dayGridPlugin from '@fullcalendar/daygrid';
+
+    let calendarEl;
+
+    onMount(() => {
+        let calendar = new Calendar(calendarEl, {
+            plugins: [dayGridPlugin],
+            initialView: 'dayGridMonth',
+            events: [
+                //Will figure out how to fetch event from server (Easy api call)
+                { title: 'HKN test 1', start: '2023-09-01' },
+                { title: 'HKN test 2', start: '2023-09-10' },
+            ],
+        });
+
+        calendar.render();
+    });
+</script>
+
+<div bind:this={calendarEl}></div>
+
