7b03ee3dfbbac7bcdbe3c6a354aa7f36ad422bbb
ryanyychen <ryan_yychen@hotmail.com>
2023-09-22 19:00:08 -0700

    Resolve conflicts

--- frontend/src/Components/Sidebar.svelte
+++ frontend/src/Components/Sidebar.svelte
@@ -38,9 +38,5 @@
   <img src={logo} alt="HKN logo" />
   <a href="/home">HomePage</a>
   <a href="/profile">Profile</a>
-<<<<<<< HEAD
-  <a href="/Logout">Logout</a>
-=======
   <a href="/accounts/logout/">Logout</a>
->>>>>>> temp_branch
 </div>
