ab812565ff7f75a9ada95981297acb3f4831d7d8
ryanyychen <ryan_yychen@hotmail.com>
2023-10-04 21:54:10 -0700

    Change title of pages

--- frontend/src/Pages/Profile.svelte
+++ frontend/src/Pages/Profile.svelte
@@ -17,108 +17,111 @@
    });
 </script>
 
-<h1 style="margin-left: 15px">Profile Page</h1> <!--TODO: Use permissions groups api to get list of groups and create a card for each group instead of doing combinations-->
-   {#if userData}
-      {#if userData.inductee_data}
-         <div class="container">
-            <h2>{userData.first_name} 
-               {#if userData.inductee_data.preferred_name != userData.first_name}({userData.inductee_data.preferred_name}) {/if}
-               {#if userData.middle_name}{userData.middle_name[0]}. {/if}
-               {userData.last_name}
-            </h2>
-            <div class="multi-row">
-               <div class="multi-column">
-                  {#if userData.outreach_data}
-                     <p>Inductee, Outreach Student</p>
-                  {:else}
-                     <p>Inductee</p>
-                  {/if}
-               </div>
-               <div class="multi-column">
-                  <h3>Email: </h3><p>{userData.email}</p>
-                  <h3>Major: </h3><p>{userData.inductee_data.major}</p>
-               </div>
-               <div class="multi-column">
-                  <h3>Degree: </h3><p>{userData.inductee_data.degree}</p>
-                  <h3>Graduation Year: </h3><p>{userData.inductee_data.grad_year}</p>
+<main>
+   <title> HKN | Profile </title>
+   <h1 style="margin-left: 15px">Profile Page</h1> <!--TODO: Use permissions groups api to get list of groups and create a card for each group instead of doing combinations-->
+      {#if userData}
+         {#if userData.inductee_data}
+            <div class="container">
+               <h2>{userData.first_name} 
+                  {#if userData.inductee_data.preferred_name != userData.first_name}({userData.inductee_data.preferred_name}) {/if}
+                  {#if userData.middle_name}{userData.middle_name[0]}. {/if}
+                  {userData.last_name}
+               </h2>
+               <div class="multi-row">
+                  <div class="multi-column">
+                     {#if userData.outreach_data}
+                        <p>Inductee, Outreach Student</p>
+                     {:else}
+                        <p>Inductee</p>
+                     {/if}
+                  </div>
+                  <div class="multi-column">
+                     <h3>Email: </h3><p>{userData.email}</p>
+                     <h3>Major: </h3><p>{userData.inductee_data.major}</p>
+                  </div>
+                  <div class="multi-column">
+                     <h3>Degree: </h3><p>{userData.inductee_data.degree}</p>
+                     <h3>Graduation Year: </h3><p>{userData.inductee_data.grad_year}</p>
+                  </div>
                </div>
             </div>
-         </div>
-         <div class="container">
-            <div class="multi-row" id="profile">
+            <div class="container">
+               <div class="multi-row" id="profile">
+                  <div>
+                     <h3>Induction Points</h3>
+                  </div>
+                  <div class="multi-column">
+                     <h3>Professional </h3><p>{userData.inductee_data.professional_points} / 1</p>
+                     <h3>Social </h3><p>{userData.inductee_data.social_points} / 2</p>
+                     <h3>Technical </h3><p>{userData.inductee_data.technical_points} / 1</p>
+                  </div>
+                  <div class="multi-column">
+                     <h3>Outreach </h3><p>{userData.inductee_data.outreach_points} / 1</p>
+                     <h3>Mentorship </h3><p>{userData.inductee_data.mentorship_points} / 1</p>
+                     <h3>General </h3><p>{userData.inductee_data.general_points}</p>
+                     <h3>Total </h3><p>{userData.inductee_data.total_points} / 10</p>
+                  </div>
+               </div>
+            </div>
+         {:else if userData.member_data}
+            <div class="container">
+               <h2>{userData.first_name} 
+                  {#if userData.member_data.preferred_name != userData.first_name}({userData.member_data.preferred_name}) {/if}
+                  {#if userData.middle_name}{userData.middle_name[0]}. {/if}
+                  {userData.last_name}
+               </h2>
+               <div class="multi-row">
+                  <div class="multi-column">
+                     {#if userData.officer_data && userData.outreach_data}
+                        <p>Officer ({userData.officer_data.position}), Outreach Student</p>
+                     {:else if userData.officer_data}
+                        <p>Officer ({userData.officer_data.position})</p>
+                     {:else if userData.outreach_data}
+                        <p>Member, Outreach Student</p>
+                     {:else}
+                        <p>Member</p>
+                     {/if}
+                  </div>
+                  <div class="multi-column">
+                     <h3>Email: </h3><p>{userData.email}</p>
+                     <h3>Major: </h3><p>{userData.member_data.major}</p>
+                  </div>
+                  <div class="multi-column">
+                     <h3>Degree: </h3><p>{userData.member_data.degree}</p>
+                     <h3>Graduation Year: </h3><p>{userData.member_data.grad_year}</p>
+                  </div>
+               </div>
+            </div>
+         {:else}
+            <div class="container">
+               <h2>{userData.first_name} {userData.last_name}</h2>
+               <div class="multi-row">
+                  <div class="multi-column">
+                     <p>Guest</p>
+                  </div>
+                  <div class="multi-column">
+                     <h3>Email: </h3><p>{userData.email}</p>
+                  </div>
+               </div>
+            </div>
+         {/if}
+         {#if userData.outreach_data}
+            <div class="container">
                <div>
-                  <h3>Induction Points</h3>
+                  <h2>Outreach Information</h2>
                </div>
-               <div class="multi-column">
-                  <h3>Professional </h3><p>{userData.inductee_data.professional_points} / 1</p>
-                  <h3>Social </h3><p>{userData.inductee_data.social_points} / 2</p>
-                  <h3>Technical </h3><p>{userData.inductee_data.technical_points} / 1</p>
-               </div>
-               <div class="multi-column">
-                  <h3>Outreach </h3><p>{userData.inductee_data.outreach_points} / 1</p>
-                  <h3>Mentorship </h3><p>{userData.inductee_data.mentorship_points} / 1</p>
-                  <h3>General </h3><p>{userData.inductee_data.general_points}</p>
-                  <h3>Total </h3><p>{userData.inductee_data.total_points} / 10</p>
+               <div class="multi-row">
+                  <div class="multi-column">
+                     <h3>Outreach course: </h3><p>{userData.outreach_data.outreach_course}</p>
+                     <h3>Hours: </h3><p>{userData.outreach_data.hours}</p>
+                     <h3>Car: </h3><p>{userData.outreach_data.car}</p>
+                  </div>
                </div>
             </div>
-         </div>
-      {:else if userData.member_data}
-         <div class="container">
-            <h2>{userData.first_name} 
-               {#if userData.member_data.preferred_name != userData.first_name}({userData.member_data.preferred_name}) {/if}
-               {#if userData.middle_name}{userData.middle_name[0]}. {/if}
-               {userData.last_name}
-            </h2>
-            <div class="multi-row">
-               <div class="multi-column">
-                  {#if userData.officer_data && userData.outreach_data}
-                     <p>Officer ({userData.officer_data.position}), Outreach Student</p>
-                  {:else if userData.officer_data}
-                     <p>Officer ({userData.officer_data.position})</p>
-                  {:else if userData.outreach_data}
-                     <p>Member, Outreach Student</p>
-                  {:else}
-                     <p>Member</p>
-                  {/if}
-               </div>
-               <div class="multi-column">
-                  <h3>Email: </h3><p>{userData.email}</p>
-                  <h3>Major: </h3><p>{userData.member_data.major}</p>
-               </div>
-               <div class="multi-column">
-                  <h3>Degree: </h3><p>{userData.member_data.degree}</p>
-                  <h3>Graduation Year: </h3><p>{userData.member_data.grad_year}</p>
-               </div>
-            </div>
-         </div>
-      {:else}
-         <div class="container">
-            <h2>{userData.first_name} {userData.last_name}</h2>
-            <div class="multi-row">
-               <div class="multi-column">
-                  <p>Guest</p>
-               </div>
-               <div class="multi-column">
-                  <h3>Email: </h3><p>{userData.email}</p>
-               </div>
-            </div>
-         </div>
+         {/if}
       {/if}
-      {#if userData.outreach_data}
-         <div class="container">
-            <div>
-               <h2>Outreach Information</h2>
-            </div>
-            <div class="multi-row">
-               <div class="multi-column">
-                  <h3>Outreach course: </h3><p>{userData.outreach_data.outreach_course}</p>
-                  <h3>Hours: </h3><p>{userData.outreach_data.hours}</p>
-                  <h3>Car: </h3><p>{userData.outreach_data.car}</p>
-               </div>
-            </div>
-         </div>
-      {/if}
-   {/if}
+</main>
 
 <style>
    .container {
