5c2aa3d188b1908d806a810fb68bd3018ebf1f63
Meghaj Vadlaputi <emailmeghaj@gmail.com>
2023-10-04 22:01:00 -0700

    Fixed dotenv stuff

--- myapp/settings.py
+++ myapp/settings.py
@@ -15,11 +15,10 @@ import os
 from django.urls import reverse_lazy
 from dotenv import load_dotenv
 
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -28,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # DEBUG = "DJANGO_DEBUG" in os.environ and os.environ["DJANGO_DEBUG"] == "ON"
 
 ALLOWED_HOSTS = ["localhost", "portal.hknucsd.com"]
