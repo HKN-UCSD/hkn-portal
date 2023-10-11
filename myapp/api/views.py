650ddb6e3883eb265c7c29337b58b2d476d8163b
Ryan Chen <103216376+ryanyychen@users.noreply.github.com>
2023-10-10 18:00:47 -0700

    Embed email image using base64 (#67)
    * Embed image using base64
    
    * Put image embedding in separate file
    
    * Import image embedding in views.py

--- myapp/api/views.py
+++ myapp/api/views.py
@@ -1,4 +1,5 @@
 import os
+import myapp.templates.registration.image_embedding as image_embedding
 from dotenv import load_dotenv
 from myapp.settings import BASE_DIR
 
@@ -367,6 +368,7 @@ def password_reset(request):
                     'domain': domain,
                     'uid': urlsafe_base64_encode(str(user.pk).encode()),
                     'token': default_token_generator.make_token(user),
+                    'hkn_vector_white': image_embedding.hkn_vector_white,
                 }
                 email_content = render_to_string('registration/password_reset_email_template.html', context)
                 message = Mail(
