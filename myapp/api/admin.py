5854ffcff1bb2c2992b01aeb58c51fda853b0238
ryanyychen <ryan_yychen@hotmail.com>
2023-11-02 23:16:08 -0700

    Register InductionClass to admin

--- myapp/api/admin.py
+++ myapp/api/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from myapp.api.models.events import Event, EventType, EventActionRecord
-from myapp.api.models.users import CustomUser
+from myapp.api.models.users import CustomUser, InductionClass
 
 # Register your models here.
 admin.site.register(Event)
 admin.site.register(EventType)
 admin.site.register(EventActionRecord)
 admin.site.register(CustomUser)
+admin.site.register(InductionClass)
