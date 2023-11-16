3033ca0ded3b5a101dca1d8ed5c5915e52a49688
ryanyychen <ryan_yychen@hotmail.com>
2023-11-15 17:25:04 -0800

    Remove code-breaking import

--- myapp/api/admin.py
+++ myapp/api/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from myapp.api.models.events import Event, EventType, EventActionRecord
 from myapp.api.models.users import CustomUser, Inductee, Member, OutreachStudent, Officer, InductionClass
 
