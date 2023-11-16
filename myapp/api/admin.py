dc78e170891709c3decea5c574969b32986d705d
ryanyychen <ryan_yychen@hotmail.com>
2023-11-15 17:23:43 -0800

    Add all user models to admin portal

--- myapp/api/admin.py
+++ myapp/api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from myapp.api.models.events import Event, EventType, EventActionRecord
-from myapp.api.models.users import CustomUser, InductionClass
+from myapp.api.models.users import CustomUser, Inductee, Member, OutreachStudent, Officer, InductionClass
+
 
 # Register your models here.
 admin.site.register(Event)
@@ -8,3 +10,7 @@ admin.site.register(EventType)
 admin.site.register(EventActionRecord)
 admin.site.register(CustomUser)
 admin.site.register(InductionClass)
+admin.site.register(Inductee)
+admin.site.register(Member)
+admin.site.register(OutreachStudent)
+admin.site.register(Officer)
