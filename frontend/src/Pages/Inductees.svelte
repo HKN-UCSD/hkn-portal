c6c7b69bb40da024896117570696dda527d7609a
andrewzpu <66804382+andrewzpu@users.noreply.github.com>
2023-11-30 21:43:41 -0800

    Updated Inductee and Outreach pages

--- frontend/src/Pages/Inductees.svelte
+++ frontend/src/Pages/Inductees.svelte
@@ -61,18 +61,19 @@ let years = [
         {"value": 'email', "title": "Email"},
         {"value": 'major', "title": 'Major'},
         {"value": 'grad_year', "title": 'Year'},
-        {"value": 'professional_points', "title": 'Pro'},
-        {"value": 'social_points', "title": 'Social'},
-        {"value": 'technical_points', "title": 'Tech'},
-        {"value": 'outreach_points', "title": 'Outreach'},
-        {"value": 'mentorship_points', "title": 'Mentor'},
-        {"value": 'general_points', "title": 'General'},
-        {"value": 'total_points', "title": 'Total Points'}
+        {"value": 'professional_points', "title": 'P'},
+        {"value": 'social_points', "title": 'S'},
+        {"value": 'technical_points', "title": 'T'},
+        {"value": 'outreach_points', "title": 'O'},
+        {"value": 'mentorship_points', "title": 'M'},
+        {"value": 'general_points', "title": 'G'},
+        {"value": 'total_points', "title": 'Total'}
     ]
 
     const sortBy = (header) => {
         if (sorting_col == header["value"]) {
             ascending = !ascending;
+
         } else {
             ascending = true;
         }
@@ -100,12 +101,57 @@ let years = [
             })
         }
     }
+    
 
     let sorting_col = "N/A";
     let ascending = true;
 
     let major_option;
     let year_option;
+
+    let csv_data;
+    
+    function tableToCSV() {
+
+        // Variable to store the final csv data
+        csv_data = [];
+
+        // Get each row data
+        var rows = document.getElementsByTagName('tr');
+        for (var i = 0; i < rows.length; i++) {
+
+            // Get each column data
+            var cols = rows[i].querySelectorAll('td,th');
+
+            // Stores each csv row data
+            var csvrow = [];
+            for (var j = 0; j < cols.length; j++) {
+
+                // Get the text data of each cell
+                // of a row and push it to csvrow
+                csvrow.push(cols[j].innerHTML);
+            }
+
+            // Combine each column value with comma
+            csv_data.push(csvrow.join(","));
+        }
+
+        // Combine each row data with new line character
+        csv_data = csv_data.join('\n');
+
+    }
+
+
+    function download_table() {
+        tableToCSV();
+        var textToSave = csv_data;
+        var hiddenElement = document.createElement('a');
+
+        hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
+        hiddenElement.target = '_blank';
+        hiddenElement.download = 'inductees.csv';
+        hiddenElement.click();
+    }
 
 </script>
 
@@ -114,7 +160,7 @@ let years = [
 </svelte:head>
 
 {#await Promise.all([getInductees(), getAdminStatus()])}
-    <div>
+    <div style="padding-left:50px">
         <h1 style="margin-left: 15px">Inductees</h1>
         <p>loading...</p>
     </div>
@@ -122,76 +168,104 @@ let years = [
 
 <main>
     {#if adminStatus}
-        <div>
+        <div style="padding-left:50px">
             <h1 style="margin-left: 15px">Inductees</h1>
-            <form>
-                <label for="majors">Filter by major:</label>
-                <select bind:value={major_option} name="majors">
-                    <option value="all">All Majors</option>
-                    {#each majors as major}
-                        <option value={major}>{major}</option>
-                    {/each}
-                </select>
-            </form>
-            <form>
-                <label for="years">Filter by Year:</label>
-                <select bind:value={year_option} name="years">
-                    <option value="all">Any</option>
-                    {#each years as year}
-                        <option value={year}>{year}</option>
-                    {/each}
-                    <option value="after">> 2026</option>
-                </select>
-            </form>
-            <table>
+            <div>
+                <form>
+                    <select bind:value={major_option} name="majors">
+                        <option value="all">Filter by Major</option>
+                        {#each majors as major}
+                            <option value={major}>{major}</option>
+                        {/each}
+                    </select>
+                </form>
+            </div>
+            <div>
+                <form>
+                    <select bind:value={year_option} name="years">
+                        <option value="all">Filter by Year</option>
+                        {#each years as year}
+                            <option value={year}>{year}</option>
+                        {/each}
+                        <option value="after">> 2027</option>
+                    </select>
+                </form>
+            </div>
+            <div>
+                <button type="button" on:click={() => download_table()}>
+                    Download as CSV
+                </button>
+            </div>
+            
+            <div id="key">
+                <div style="padding:0px">
+                    <h3 id="side">Key</h3>
+                </div>
+                <div style="padding-bottom:0px">
+                    <h3>Point Categories</h3>
+                    <p>P - Professional</p>
+                    <p>S - Social</p>
+                    <p>T - Technology</p>
+                    <p>O - Outreach</p>
+                    <p>M - Mentorship</p>
+                    <p>G - General</p>
+                </div>
+            </div>
+            <table id="inducteeTable">
                 <tr>
                     {#each headers as header}
-                        <th on:click={() => sortBy(header)}>{header["title"]}</th>
+                        {#if (sorting_col != header['value'])}
+                            <th on:click={() => sortBy(header)}>{header["title"]}</th>
+                        {:else if (ascending)}
+                            <th on:click={() => sortBy(header)}>{header["title"]}⏶</th>
+                        {:else}
+                            <th on:click={() => sortBy(header)}>{header["title"]}⏷</th>
+                        {/if}
                     {/each}
                 </tr>
-            {#each inducteesData as inducteeData}
-                {#if (major_option == "all" || inducteeData.major == major_option)
-                    && (year_option == "all" || inducteeData.grad_year == parseInt(year_option) || (inducteeData.grad_year > 2027 && year_option == "after"))}
-                    <tr>
-                        <td>
-                            {inducteeData.preferred_name}
-                        </td>
-                        <td>
-                            {inducteeData.last_name}
-                        </td>
-                        <td>
-                            {inducteeData.email}
-                        </td>
-                        <td>
-                            {inducteeData.major}
-                        </td>
-                        <td>
-                            {inducteeData.grad_year}
-                        </td>
-                        <td>
-                            {inducteeData.professional_points}
-                        </td>
-                        <td>
-                            {inducteeData.social_points}
-                        </td>
-                        <td>
-                            {inducteeData.technical_points}
-                        </td>
-                        <td>
-                            {inducteeData.outreach_points}
-                        </td>
-                        <td>
-                            {inducteeData.mentorship_points}
-                        </td>
-                        <td>
-                            {inducteeData.general_points}
-                        </td>
-                        <td>
-                            {inducteeData.total_points}
-                        </td>
-                    </tr>
-                {/if}
-            {/each}
+                {#each inducteesData as inducteeData}
+                    {#if (major_option == "all" || inducteeData.major == major_option)
+                        && (year_option == "all" || inducteeData.grad_year == parseInt(year_option) || (inducteeData.grad_year > 2027 && year_option == "after"))}
+                        <tr>
+                            <td>
+                                {inducteeData.preferred_name}
+                            </td>
+                            <td>
+                                {inducteeData.last_name}
+                            </td>
+                            <td>
+                                {inducteeData.email}
+                            </td>
+                            <td>
+                                {inducteeData.major}
+                            </td>
+                            <td>
+                                {inducteeData.grad_year}
+                            </td>
+                            <td style="text-align: center">
+                                {inducteeData.professional_points}
+                            </td>
+                            <td style="text-align: center">
+                                {inducteeData.social_points}
+                            </td>
+                            <td style="text-align: center">
+                                {inducteeData.technical_points}
+                            </td>
+                            <td style="text-align: center">
+                                {inducteeData.outreach_points}
+                            </td>
+                            <td style="text-align: center">
+                                {inducteeData.mentorship_points}
+                            </td>
+                            <td style="text-align: center">
+                                {inducteeData.general_points}
+                            </td>
+                            <td style="text-align: center">
+                                {inducteeData.total_points}
+                            </td>
+                        </tr>
+                    {/if}
+                {/each}
             </table>
         </div>
     {:else}
@@ -205,27 +279,84 @@ let years = [
 
 <style>
     div {
-        padding: 30px;
-        padding-top: 10px;
-        margin: 30px;
+        float:left;
+        padding: 20px;
+        padding-top: 0px;
     }
-    table, th, td{
+    table {
         /* border: 1px solid grey; */
-        border: none;
+        border-radius:20px;
+        border:solid gray 1px;
+        border-collapse: separate;
+        height: 60%;
+        overflow:hidden;
+        border-spacing:0;
+        float:left;
+    }
+    th {
         border-collapse: collapse;
-    }
-
-    td{
-        padding: 10px 15px;
-    }
-
-    th {
-        padding: 15px;
-        /* border-spacing: 5px; */
-    }
-
-    th {
-        background-color: var(--fc-button-bg-color);
+        padding: 10px;
+        background-color: rgb(44,62,80);
         color: white;
+        text-transform: capitalize;
+        width:5%;
     }
+    th:hover {
+        cursor: pointer;
+        background-color: rgb(24,42,60);
+    }
+    th:nth-child(1) {
+        width: 10%;
+    }
+    th:nth-child(2) {
+        width: 10%;
+    }
+    th:nth-child(3) {
+        width: 15%;
+    }
+    th:nth-child(4) {
+        width: 15%;
+    }
+    th:nth-child(5) {
+        width: 6%;
+    }
+    th:nth-child(12) {
+        width: 7%;
+    }
+    tr:nth-child(odd) {
+        background-color: rgb(240,240,255);
+    }
+    td {
+        padding: 10px;
+        overflow: wrap;
+    }
+    p, h3 {
+        padding:0px;
+        margin:0px;
+    }
+    #key {
+        position:fixed;
+        top:0px;
+        right:-220px;
+        border: solid black 1px;
+        border-radius: 20px;
+        background-color: white;
+        padding:20px 40px 20px 5px;
+        transition: 1s;
+    }
+    #key:hover {
+        right: -20px;
+        transition: 1s;
+    }
+    #key:hover #side {
+        top: -100px;
+        transition: 1s;
+    }
+    #side {
+        top: 0px;
+        transition: 1s;
+        position:relative;
+        transform:rotate(-90deg);
+    }
+
 </style>
