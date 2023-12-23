3d00f1936169d9a34a78e41ef82acb1ba808ac20
林耕廉 <keng@hayashikourens-Laptop.local>
2023-12-23 13:48:00 -0800

    added default banner file(canvaEmbed.js), implement default event type banner, update event detail content page as well

---
+++ frontend/src/Components/Events/canvaEmbed.js
@@ -0,0 +1,66 @@
+const technical = `
+<div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%; \
+ padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); \
+ margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; \
+ border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; \
+ top: 0; left: 0; border: none; padding: 0;margin: 0;" \
+ src="https://www.canva.com/design/DAF31CiRfDY/view?embed" \
+ allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+<a href="https://www.canva.com/design/DAF31CiRfDY/view?utm_content=DAF31CiRfDY&utm_campaign=designshare&utm_medium=embeds&utm_source=link" \
+ target="_blank" rel="noopener">Technical_Event</a> by Lin Roger
+`;
+
+const professional = `
+<div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%; \
+ padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); \
+ margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; \
+ border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; \
+ top: 0; left: 0; border: none; padding: 0;margin: 0;" \
+ src="https://www.canva.com/design/DAF31FbDHkU/view?embed" \
+ allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+<a href="https://www.canva.com/design/DAF31FbDHkU/view?utm_content=DAF31FbDHkU&utm_campaign=designshare&utm_medium=embeds&utm_source=link" \
+ target="_blank" rel="noopener">Professional_Event</a> by Lin Roger
+`;
+
+const social = `
+<div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%; \
+ padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); \
+ margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; \
+ border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; \
+ top: 0; left: 0; border: none; padding: 0; margin: 0;" \
+ src="https://www.canva.com/design/DAF31ATbzRs/view?embed" \
+ allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+<a href="https://www.canva.com/design/DAF31ATbzRs/view?utm_content=DAF31ATbzRs&utm_campaign=designshare&utm_medium=embeds&utm_source=link" \
+ target="_blank" rel="noopener">Social_Event</a> by Lin Roger
+`;
+
+const mentorship = `
+<div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%; \
+ padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); \
+ margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; \
+ border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; \
+ top: 0; left: 0; border: none; padding: 0; margin: 0;" \
+ src="https://www.canva.com/design/DAF31MMZfHw/view?embed" \
+ allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+<a href="https://www.canva.com/design/DAF31MMZfHw/view?utm_content=DAF31MMZfHw&utm_campaign=designshare&utm_medium=embeds&utm_source=link" \
+ target="_blank" rel="noopener">Mentorship_Event</a> by Lin Roger
+`;
+
+export const embedCode = {
+  "Technical" : technical,
+  "Professional" : professional,
+  "Mentorship" : mentorship,
+  "Social" : social,
+}
