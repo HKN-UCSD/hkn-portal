508376a13c79ff78b5bae08cb8d988ec39079f3c
Keng Lien(Roger) Lin <98373624+magikarp620@users.noreply.github.com>
2024-01-10 18:34:35 -0800

    Default banner for different event type (#112)
    * added default banner file(canvaEmbed.js), implement default event type banner, update event detail content page as well
    
    * Change canva embed code to HKN version
    
    ---------
    
    Co-authored-by: 林耕廉 <keng@hayashikourens-Laptop.local>

---
+++ frontend/src/Components/Events/canvaEmbed.js
@@ -0,0 +1,35 @@
+const technical = `<div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%; padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0; margin: 0;"
+    src="https://www.canva.com/design/DAF5a2Q6B7o/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+<a href="https://www.canva.com/design/DAF5a2Q6B7o/view?utm_content=DAF5a2Q6B7o&utm_campaign=designshare&utm_medium=embeds&utm_source=link" target="_blank" rel="noopener">Technical</a> by HKN Kappa Psi`;
+
+const professional = `<div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%; padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0; margin: 0;"
+    src="https://www.canva.com/design/DAF5a-v4zys/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+<a href="https://www.canva.com/design/DAF5a-v4zys/view?utm_content=DAF5a-v4zys&utm_campaign=designshare&utm_medium=embeds&utm_source=link" target="_blank" rel="noopener">Professional Event</a> by HKN Kappa Psi`;
+
+const social = `<div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%; padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0; margin: 0;"
+    src="https://www.canva.com/design/DAFSjPBOyW0/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+<a href="https://www.canva.com/design/DAFSjPBOyW0/view?utm_content=DAFSjPBOyW0&utm_campaign=designshare&utm_medium=embeds&utm_source=link" target="_blank" rel="noopener">Events</a> by HKN Kappa Psi`;
+
+
+const mentorship = `<div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%; padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0; margin: 0;"
+    src="https://www.canva.com/design/DAF5a6OveXs/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+<a href="https://www.canva.com/design/DAF5a6OveXs/view?utm_content=DAF5a6OveXs&utm_campaign=designshare&utm_medium=embeds&utm_source=link" target="_blank" rel="noopener">Mentorship</a> by HKN Kappa Psi`;
+
+export const embedCode = {
+  "Technical" : technical,
+  "Professional" : professional,
+  "Mentorship" : mentorship,
+  "Social" : social,
+}
