af28979a52ab3ea9d6c818591593bf8395d5936e
ryanyychen <ryan_yychen@hotmail.com>
2024-01-17 22:12:13 -0800

    Create car pool when dragging driver to spot

--- frontend/src/Pages/Events/OutreachRides.svelte
+++ frontend/src/Pages/Events/OutreachRides.svelte
@@ -1,60 +1,67 @@
 <script>
-    import { loop_guard } from "svelte/internal";
     import { getEvent } from "../../Components/Events/eventstore";
     import { onMount } from "svelte";
-    import { readable } from "svelte/store";
+    import { readable, writable } from "svelte/store";
     export let id;
 
-    async function getAttendees(event) {
-        let response = await fetch(`/api/eventactionrecords/`);
-        if (response.status == 200) {
-            // filter for RSVP records related to this event
-            let records = await response.json();
-            let attendees = [];
-            for (let record of records) {
-                if (record.event == event.pk && record.action == "RSVP") {
-                    attendees.push(record.acted_on);
+    export const functionStore = writable({
+        getAttendees: async(event) => {
+            let response = await fetch(`/api/eventactionrecords/`);
+            if (response.status == 200) {
+                // filter for RSVP records related to this event
+                let records = await response.json();
+                let attendees = [];
+                for (let record of records) {
+                    if (record.event == event.pk && record.action == "RSVP") {
+                        attendees.push(record.acted_on);
+                    }
                 }
+                return attendees;
+            } else {
+                throw new Error(response.statusText);
             }
-            return attendees;
-        } else {
-            throw new Error(response.statusText);
-        }
-    }
+        },
 
-    async function getOutreachStudents() {
-        let response = await fetch(`/api/outreach/`);
-        return await response.json();
-    }
+        getOutreachStudents: async() => {
+            let response = await fetch(`/api/outreach/`);
+            return await response.json();
+        },
+        getUsers: async() => {
+            let response = await fetch(`/api/users/`);
+            return await response.json();
+        },
+    })
 
-    async function getUsers() {
-        let response = await fetch(`/api/users/`);
-        return await response.json();
-    }
+    export const varStore = writable({
+        event: null,
+        attendees: [],
+        outreachStudents: [],
+        users: [],
+    })
 
-    // Grab all attendees and outreach students
-    // Sort into 2 lists: drivers and passengers
+    const { getAttendees, getOutreachStudents, getUsers } = $functionStore;
+
     let drivers = [];
     let passengers = [];
-    let event;
-    let attendees;
-    let outreachStudents;
-    let users;
 
     const loading = readable(true, (set) => {
         onMount(async () => {
-            event = await getEvent(id);
-            attendees = await getAttendees(event);
-            outreachStudents = await getOutreachStudents();
-            users = await getUsers();
+            const event = await getEvent(id);
+            const attendees = await getAttendees(event);
+            const outreachStudents = await getOutreachStudents();
+            const users = await getUsers();
+
+            varStore.update(() => ({
+                event,
+                attendees,
+                outreachStudents,
+                users,
+            }));
 
             if (attendees) {
                 for (let attendee of attendees) {
-                    console.log(attendee);
                     let user = users.find(s => s.user_id === attendee);
                     if (user) {
-                        console.log(user);
-                        console.log(user.first_name);
                         if (outreachStudents.includes(attendee)) {
                             let student = outreachStudents.find(s => s.id === attendee.id)
                             if (student.car == "Yes") {
@@ -81,49 +88,148 @@
 {#if $loading}
     <p>Loading...</p>
 {:else}
+    <!-- Drag and Drop script -->
+    <script>
+        async function getUsers() {
+            let response = await fetch(`/api/users/`);
+            return await response.json();
+        }
+
+        function drag(ev) {
+            ev.dataTransfer.setData("id", ev.target.id);
+            ev.dataTransfer.setData("origin", ev.target.parentNode.id);
+        }
+
+        function allowDrop(ev) {
+            ev.preventDefault();
+        }
+
+        function drop(ev) {
+            ev.preventDefault();
+            let element_id = ev.dataTransfer.getData("id");
+            let element = document.getElementById(element_id);
+            if (ev.target.nodeName != "P") {
+                console.log("not p element");
+                ev.target.appendChild(document.getElementById(data));
+            }
+        }
+
+        function dropCar(ev) {
+            ev.preventDefault();
+            let element_id = ev.dataTransfer.getData("id");
+            let element = document.getElementById(element_id);
+            if (element.getAttribute("hasCar") == "true") {
+                ev.target.appendChild(document.getElementById(element_id));
+            } else {
+                let origin_id = ev.dataTransfer.getData("origin");
+                let origin = document.getElementById(origin_id);
+                origin.appendChild(document.getElementById(element_id));
+            }
+        }
+
+        function dropNoCar(ev) {
+            ev.preventDefault();
+            let element_id = ev.dataTransfer.getData("id");
+            let element = document.getElementById(element_id);
+            if (element.getAttribute("hasCar") == "false") {
+                ev.target.appendChild(document.getElementById(element_id));
+            } else {
+                let origin_id = ev.dataTransfer.getData("origin");
+                let origin = document.getElementById(origin_id);
+                origin.appendChild(document.getElementById(element_id));
+            }
+        }
+
+        function dropNewPool(ev) {
+            ev.preventDefault();
+            let data = ev.dataTransfer.getData("id");
+            let element = document.getElementById(data);
+            if (element.getAttribute("hasCar") == "true") {
+                // create new element
+                let newCarPool = document.createElement("div");
+                newCarPool.style.cssText = `
+                    outline: 2px solid black;
+                    border-radius: 10px;
+                    display: flex;
+                    flex-direction: column;
+                    margin: 10px;
+                    padding: 5px;
+                    `;
+                
+                let driverHeading = document.createElement("p");
+                driverHeading.style.cssText = `
+                    font-size: 20px;
+                    font-weight: bold;
+                    margin: 5px 5px 5px 5px;
+                    `;
+                driverHeading.innerHTML = "Driver";
+                newCarPool.appendChild(driverHeading);
+
+                let driverBox = document.createElement("div");
+                driverBox.setAttribute("ondrop", "dropCar(event)");
+                driverBox.setAttribute("ondragover", "allowDrop(event)");
+                driverBox.style.cssText = `
+                    display: flex;
+                    flex-direction: column;
+                    min-height: 20px;
+                `;
+                driverBox.appendChild(document.getElementById(data));
+                newCarPool.appendChild(driverBox);
+
+                let passengerHeading = document.createElement("p");
+                passengerHeading.style.cssText = `
+                    font-size: 20px;
+                    font-weight: bold;
+                    margin: 5px 5px 5px 5px;
+                    `;
+                passengerHeading.innerHTML = "Passengers";
+                newCarPool.appendChild(passengerHeading)
+
+                let passengerBox = document.createElement("div");
+                passengerBox.setAttribute("ondrop", "drop(event)");
+                passengerBox.setAttribute("ondragover", "allowDrop(event)");
+                passengerBox.style.cssText = `
+                    display: flex;
+                    flex-direction: column;
+                    min-height: 20px;
+                `;
+                newCarPool.appendChild(passengerBox);
+
+                let ridesContainer = document.getElementById("rides");
+                ridesContainer.insertBefore(newCarPool, ridesContainer.firstChild);
+                
+            } else {
+                document.getElementById("noCars").appendChild(document.getElementById(data));
+            }
+        }
+    </script>
     <main>
-        <div id = "page">
+        <div id="page">
             <!-- Left part of page to display every RSVP'd person -->
-            <section id = "people">
-                <section id = "cars" ondrop="drop(event)" ondragover="allowDrop(event)">
-                    <h2>Drivers</h2>
+            <section id="people">
+                <h2>Drivers</h2>
+                <section id="cars" ondrop="dropCar(event)" ondragover="allowDrop(event)">
                     {#each drivers as driver}
-                        <p id="car" draggable="true" ondragstart="drag(event)"> {driver.first_name} {driver.last_name} ({driver.email})</p>
+                        <p class="attendee" id="{driver.email}" hasCar="true" draggable="true" ondragstart="drag(event)" ondrop="drop(event)"> {driver.first_name} {driver.last_name} ({driver.email})</p>
                     {/each}
+                    <p id="yyc003@ucsd.edu" hasCar="true" draggable="true" ondragstart="drag(event)"> Ryan Chen (yyc003@ucsd.edu) </p>
                 </section>
-                <section id = "no_cars" ondrop="drop(event)" ondragover="allowDrop(event)">
-                    <h2>Attendees</h2>
+                <h2>Attendees</h2>
+                <section id="noCars" ondrop="dropNoCar(event)" ondragover="allowDrop(event)">
                     {#each passengers as passenger}
-                        <p id="car" draggable="true" ondragstart="drag(event)"> {passenger.first_name} {passenger.last_name} ({passenger.email})</p>
+                        <p class="attendee" id="{passenger.email}" hasCar="false" draggable="true" ondragstart="drag(event)" ondragover="" ondrop=""> {passenger.first_name} {passenger.last_name} ({passenger.email})</p>
                     {/each}
                 </section>
             </section>
 
             <!-- Right part of page to plan out rides -->
-            <section id = "rides">
+            <section id="rides">
                 <!-- Create new "CarBox" when a driver is dropped into this box-->
-                <div id = "newCarBox" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
-                <script> 
-                    // on drop create new box with the name of the driver
-                </script>
+                <div id="newCarBox" ondrop="dropNewPool(event)" ondragover="allowDrop(event)">
+                    <p style="text-align:center; color: white;">Drop drivers here to create a new carpool</p>
+                </div>
             </section>
         </div>
-
-        <!-- Drag and Drop script -->
-        <script>
-            function allowDrop(ev) {
-            ev.preventDefault();
-            }
-            function drag(ev) {
-            
-            ev.dataTransfer.setData("text", ev.target.id);
-            }
-            function drop(ev) {
-            ev.preventDefault();
-            let data = ev.dataTransfer.getData("text");
-            ev.target.appendChild(document.getElementById(data));
-            }
-        </script>
     </main>
 {/if}
 
@@ -146,18 +252,39 @@
     }
     #cars{
         outline: 1px solid red;
-        margin: 10px 0px 0px 10px;
+        margin: 10px;
         height: 50vh;
     }
-    #no_cars{
+    #noCars{
         outline: 1px solid red;
-        margin: 10px 0px 0px 10px;
+        margin: 10px;
         height: 50vh;
+    }
+    #rides{
+        display: flex;
+        flex-direction: column;
+        align-content: center;
     }
     #newCarBox{
-        outline: 1px solid blue;
-        margin: 10px 0px 0px 10px;
+        outline: 2px solid black;
+        border-radius: 10px;
+        background-color: #099c30;
+        opacity: 0.7;
+        display: flex;
+        margin: 10px 10px 10px 10px;
         width: 200px;
         height: 50px;
+        justify-content: center;
+        align-content: center;
+    }
+    .attendee{
+        outline: 1px solid black;
+        border-radius: 5px;
+    }
+    h2{
+        margin: 10px;
+    }
+    p{
+        margin: 5px;
     }
 </style>
