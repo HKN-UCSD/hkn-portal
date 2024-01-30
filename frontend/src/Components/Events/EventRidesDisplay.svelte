b596da17e500a0a92770fe244e3def9673423244
ryanyychen <ryan_yychen@hotmail.com>
2024-01-29 19:42:07 -0800

    Check for RSVP status before displaying rides
    Check whether the current user has RSVP'd for the event before displaying ride assignment.
    Will not display ride assignment if user has already been assigned a ride but has un-RSVP'd.

--- frontend/src/Components/Events/EventRidesDisplay.svelte
+++ frontend/src/Components/Events/EventRidesDisplay.svelte
@@ -3,32 +3,65 @@
 <script>
     export let event;
 
-    async function getUserRides(event) {
+    // Determine if the current user has RSVP'd for the event
+    // Parameters: none
+    // Output: true if user has RSVP'd, false otherwise
+    async function RSVPd() {
+        const user = await (await fetch(`/api/profile/`)).json();
+        const actionRecords = await (await fetch(`/api/eventactionrecords/`)).json();
+        const eventRSVPs = actionRecords.filter(record => record.event == event.pk && record.action == "RSVP");
+
+        if (eventRSVPs.find(record => record.acted_on == user.user_id)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // Gets the carpool that the current user is in
+    // Parameters: none
+    // Output: a JSON object representing the carpool the user is in
+    //         format of JSON object: {"driver": attendee, "passengers": [list of attendees]}
+    async function getUserRides() {
+        // Check if user is RSVP'd
+        const RSVP = await RSVPd();
+        if (!RSVP) {
+            return null;
+        }
+
+        // Define function to parse email from "First Last (Email)"
+        function parseEmail(attendee) {
+            return attendee.split("(")[1].split(")")[0];
+        }
+
         const rides = event.rides;
         const user = await (await fetch(`/api/profile/`)).json();
 
-        let userPool;
-        // Return entire carpool user is in
+        let userPool = null;
+
+        // Set userPool to the carpool that the current user is in
         for (const key in rides) {
             if (rides[key]["driver"]) {
-                if (rides[key]["driver"] == user.email) {
-                    userPool = rides[key];
-                    break;
+                const driverEmail = parseEmail(rides[key]["driver"]);
+                if (driverEmail == user.email) {
+                    return rides[key];
                 }
             }
-            for (const email of rides[key]["passengers"]) {
-                if (email == user.email) {
-                    userPool = rides[key];
-                    break;
+            for (const passenger of rides[key]["passengers"]) {
+                const passengerEmail = parseEmail(passenger);
+                if (passengerEmail == user.email) {
+                    return rides[key]
                 }
             }
         }
+
+        // null if user is not in any carpool
         return userPool;
     }
 </script>
 
 {#if event != null}
-    {#await getUserRides(event)}
+    {#await getUserRides()}
         <p>Loading...</p>
     {:then userRides}
         {#if userRides != null}
@@ -67,7 +100,7 @@
     table, th, td{
         border: none;
         border-collapse: collapse;
-        width: 20%;
+        max-width: 40%;
     }
 
     td{
