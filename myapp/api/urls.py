f8e60d2fece2782ef2d024a56d771b84637ad8d9
ryanyychen <ryan_yychen@hotmail.com>
2024-02-09 01:09:12 -0800

    Register Major and DegreeLevel to api urls
    Also reorder some router registrations

--- myapp/api/urls.py
+++ myapp/api/urls.py
@@ -6,16 +6,20 @@ from django.conf.urls.static import static
 from myapp.api.views import event_views, user_views
 
 router = DefaultRouter()
-router.register(r'events', event_views.EventViewSet, basename="event")
 router.register(r'eventactionrecords', event_views.EventActionRecordViewSet, basename='eventrecord')
+router.register(r'events', event_views.EventViewSet, basename="event")
+router.register(r'eventtypes', event_views.EventTypeViewSet, basename="eventtype")
+
+router.register(r'groups', user_views.GroupsViewSet, basename="groups")
 router.register(r'users', user_views.UserViewSet, basename="users")
+router.register(r'majors', user_views.MajorViewSet, basename="major")
+router.register(r'degreelevel', user_views.DegreeLevelViewSet, basename="degree")
 router.register(r'profile', user_views.UserProfileViewSet, basename="profile")
-router.register(r'officers', user_views.OfficerViewSet, basename="officers")
 router.register(r'inductees', user_views.InducteeViewSet, basename="inductees")
 router.register(r'inductionclasses', user_views.InductionClassViewSet, basename="inductionclass")
 router.register(r'outreach', user_views.OutreachViewSet, basename="outreach")
-router.register(r'eventtypes', event_views.EventTypeViewSet, basename="eventtype")
-router.register(r'groups', user_views.GroupsViewSet, basename="groups")
+router.register(r'officers', user_views.OfficerViewSet, basename="officers")
+
 
 urlpatterns = [
    path("actions/", event_views.EventActionView),
