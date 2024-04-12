823026a2c82605c4d5ff0c89c0d3d562d47a95f5
Meghaj Vadlaputi <45153120+MeghajV@users.noreply.github.com>
2024-04-11 22:26:28 -0700

    Feedback hovering button (#151)

---
+++ frontend/src/Components/Feedback.svelte
@@ -0,0 +1,23 @@
+<style>
+  .feedback-button {
+    position: fixed;
+    bottom: 10px;
+    right: 10px;
+    z-index: 999;
+  }
+
+  .feedback-button button {
+    background-color: #333;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+</style>
+
+<div class="feedback-button" id="feedbackButton">
+    <a href="https://forms.gle/9hQvsE2yNF9iRyF4A" target="_blank">
+      <button>Feedback?</button>
+    </a>
+</div>
