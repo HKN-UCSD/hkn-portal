9d912b8018049ccb27ad3f292cac47c006d76618
ryanyychen <ryan_yychen@hotmail.com>
2024-04-17 21:11:19 -0700

    Briefly set up skeleton of Houses page

---
+++ frontend/src/Pages/Houses.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Layout from "../Layout.svelte";
+    import { onMount } from "svelte";
+
+    onMount(async () => {
+        // use Google API to access house sheet
+        // https://developers.google.com/sheets/api/guides/concepts
+    });
+</script>
+<svelte:head>
+    <title> HKN Portal | Houses </title>
+</svelte:head>
+<Layout>
+    <main>
+        <div style="padding-left:50px">
+            <h1>Houses</h1>
+            <p>Coming soon!</p>
+        </div>
+    </main>
+</Layout>
