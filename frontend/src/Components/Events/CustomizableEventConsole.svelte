6b660e4b0213b7ac0f7f9efb495b3210d7a1c151
andrewzpu <66804382+andrewzpu@users.noreply.github.com>
2024-04-23 21:51:13 -0700

    Added download button for event attendance table

--- frontend/src/Components/Events/CustomizableEventConsole.svelte
+++ frontend/src/Components/Events/CustomizableEventConsole.svelte
@@ -185,6 +185,52 @@
     // generate a console table
     let selectedProperties = ["Name", "Check Off", "Points", "Edit Points", "Sign In Time"];
     filters = [(row) => row["Sign In Time"] != undefined];
+
+    let csv_data;
+
+    function tableToCSV() {
+
+        // Variable to store the final csv data
+        csv_data = [];
+
+        // Get each row data
+        var rows = document.getElementsByTagName('tr');
+        var cols = rows[0].querySelectorAll('td,th');
+
+        for (var i = 0; i < rows.length; i++) {
+
+            // Get each column data
+            cols = rows[i].querySelectorAll('td,th');
+
+            // Stores each csv row data
+            var csvrow = [];
+            for (var j = 0; j < cols.length; j++) {
+                if (typeof sortedRows[0][selectedProperties[j]] != "object") {
+                    csvrow.push(cols[j].innerHTML);
+                } 
+            }
+
+            // Combine each column value with comma
+            csv_data.push(csvrow.join(","));
+        }
+
+        // Combine each row data with new line character
+        csv_data = csv_data.join('\n');
+
+    }
+
+
+    function download_table() {
+        tableToCSV();
+        var textToSave = csv_data;
+        var hiddenElement = document.createElement('a');
+
+        hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
+        hiddenElement.target = '_blank';
+        hiddenElement.download = 'inductees.csv';
+        hiddenElement.click();
+    }
+
 </script>
 
 <!-- Event Action Bar -->
@@ -288,6 +334,11 @@
                     </tr>
                 {/each}
             </table>
+
+            <button id="downloadButton" type="button" on:click={() => download_table()}>
+                Download as CSV
+            </button>
+
             {#if modalUserData}
                 <Modal bind:modalUserData />
             {/if}
