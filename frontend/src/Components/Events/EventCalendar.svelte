6f60c9c8bbe4d3e6fa985606529e5e8182f4a544
ryanyychen <ryan_yychen@hotmail.com>
2024-05-16 19:38:10 -0700

    Initial color palette

--- frontend/src/Components/Events/EventCalendar.svelte
+++ frontend/src/Components/Events/EventCalendar.svelte
@@ -11,6 +11,40 @@
     onMount(async () => {
         const calendarEl = document.getElementById('calendar');
 
+        var eventColors = {
+            'General': '#216CDE',
+            'Mentorship': '#5887FF',
+            'Professional': '#22629D',
+            'Technical': '#715AFF',
+            'Social': '#A682FF',
+            'Outreach': '#55C1FF'
+        }
+        // var eventColors = {
+        //     'General': '#216CDE',
+        //     'Mentorship': '#BB444F',
+        //     'Professional': '#597c60',
+        //     'Technical': '#9318E7',
+        //     'Social': '#1E6091',
+        //     'Outreach': '#168AAD'
+        // }
+        // var eventColors = {
+        //     'General': '#184E77',
+        //     'Mentorship': '#1A759F',
+        //     'Professional': '#34A0A4',
+        //     'Technical': '#52B69A',
+        //     'Social': '#99D98C',
+        //     'Outreach': '#D9ED92'
+        // }
+
+        var eventBorders = {
+            'General': '#216CDE',
+            'Mentorship': '#48CAE4',
+            'Professional': '#597c60',
+            'Technical': '#52B69A',
+            'Social': '#1E6091',
+            'Outreach': '#168AAD'
+        }
+
         // Fetch events from the API and wait for the promise to resolve
         let events = await getEvents();
 
@@ -34,8 +68,9 @@
                 end: event.end_time,
                 url: `/events/${event.pk}`,
                 content: event.descriptions,
-                backgroundColor: event.is_draft ? '#bababa' : '#4350AF',
-                borderColor: event.is_draft ? '#bababa' : '#4350AF',
+                backgroundColor: event.is_draft ? '#bababa' : eventColors[event.event_type],
+                borderColor: event.is_draft ? '#bababa' : eventColors[event.event_type],
+                textColor: '#FFFFFF',
             })),
             eventDisplay: 'block',
             customButtons: {
