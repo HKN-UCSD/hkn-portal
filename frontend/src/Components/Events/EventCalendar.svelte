b45bc4fad0a319384b58c09239e28e17aabbdb08
ryanyychen <ryan_yychen@hotmail.com>
2024-10-29 17:53:23 -0700

    Remove commented color schemes

--- frontend/src/Components/Events/EventCalendar.svelte
+++ frontend/src/Components/Events/EventCalendar.svelte
@@ -13,36 +13,11 @@
 
         var eventColors = {
             'General': '#216CDE',
-            'Mentorship': '#5887FF',
-            'Professional': '#22629D',
-            'Technical': '#715AFF',
-            'Social': '#A682FF',
-            'Outreach': '#55C1FF'
-        }
-        // var eventColors = {
-        //     'General': '#216CDE',
-        //     'Mentorship': '#BB444F',
-        //     'Professional': '#597c60',
-        //     'Technical': '#9318E7',
-        //     'Social': '#1E6091',
-        //     'Outreach': '#168AAD'
-        // }
-        // var eventColors = {
-        //     'General': '#184E77',
-        //     'Mentorship': '#1A759F',
-        //     'Professional': '#34A0A4',
-        //     'Technical': '#52B69A',
-        //     'Social': '#99D98C',
-        //     'Outreach': '#D9ED92'
-        // }
-
-        var eventBorders = {
-            'General': '#216CDE',
-            'Mentorship': '#48CAE4',
-            'Professional': '#597c60',
-            'Technical': '#52B69A',
-            'Social': '#1E6091',
-            'Outreach': '#168AAD'
+            'Mentorship': '#216CDE',
+            'Professional': '#216CDE',
+            'Technical': '#216CDE',
+            'Social': '#216CDE',
+            'Outreach': '#06402B'
         }
 
         // Fetch events from the API and wait for the promise to resolve
