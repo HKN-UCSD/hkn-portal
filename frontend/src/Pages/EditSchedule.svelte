af863469e954ce1ebc8aad0892e0a095c1a2ed0b
ryanyychen <ryan_yychen@hotmail.com>
2024-11-28 17:51:14 -0800

    Complete edit schedule page

--- frontend/src/Pages/EditSchedule.svelte
+++ frontend/src/Pages/EditSchedule.svelte
@@ -4,6 +4,7 @@
     import { generateSchedule, UNAVAILABLE_COLOR, AVAILABLE_COLOR, NUM_DAYS, NUM_SLOTS } from "./interviewscheduleutils.js";
 
     let availability;
+    let selecting = null;
 
     onMount(async () => {
         await getAvailability();
@@ -11,7 +12,63 @@
         if (availability != null) {
             populateSchedule();
         }
+
+        document.querySelectorAll('.timeslot').forEach(timeslot => {
+            timeslot.setAttribute('draggable', true);
+
+            timeslot.addEventListener('click', (event) => {
+                if (event.target.getAttribute('available') == 'false') {
+                    event.target.setAttribute('available', true);
+                    event.target.style.background = AVAILABLE_COLOR;
+                } else if (event.target.getAttribute('available') == 'true') {
+                    event.target.setAttribute('available', false);
+                    event.target.style.background = UNAVAILABLE_COLOR;
+                }
+            });
+
+            timeslot.addEventListener('mouseenter', (event) => {
+                if (event.buttons === 1) {
+                    toggleTimeslot(timeslot);
+                }
+            });
+        });
+
+        document.addEventListener('dragstart', (event) => {
+            event.preventDefault();
+            if (event.target.classList.contains('timeslot')) {
+                let timeslot = event.target;
+                if (timeslot.getAttribute('available') == 'false') {
+                    selecting = true;
+                } else if (timeslot.getAttribute('available') == 'true') {
+                    selecting = false;
+                }
+                toggleTimeslot(timeslot);
+            }
+        });
     });
+
+    function toggleTimeslot(timeslot) {
+        if (selecting == null) {
+            return;
+        }
+        if (timeslot.getAttribute('available') == selecting) {
+            return;
+        }
+
+        let day = timeslot.id.split('-')[0];
+        let slotNum = timeslot.id.split('-')[1];
+
+        if (selecting == true) {
+            console.log('selecting: true');
+            timeslot.style.background = AVAILABLE_COLOR;
+            availability[day][slotNum] = 1;
+        } else {
+            console.log('selecting: false');
+            timeslot.style.background = UNAVAILABLE_COLOR;
+            availability[day][slotNum] = 0;
+        }
+        timeslot.setAttribute('available', selecting);
+    }
 
     // Make an api call to backend to retrieve availability
     async function getAvailability() {
@@ -23,19 +80,45 @@
         }
     }
 
-    // Populate the schedule with availability
-    function addAvailability() {
-        for (let i = 0; i < NUM_DAYS; i++) {
-            for (let j = 0; j < NUM_SLOTS; j++) {
-                const cell = document.getElementById(`cell-${i}-${j}`);
-                if (availability[i][j] === 0) {
-                    cell.style.backgroundColor = UNAVAILABLE_COLOR;
+    function populateSchedule() {
+        for (let day = 0; day < NUM_DAYS; day++) {
+            for (let slotNum = 0; slotNum < NUM_SLOTS; slotNum++) {
+                let timeslot = document.getElementById(`${day}-${slotNum}`);
+                if (availability[day][slotNum] == 1) {
+                    timeslot.style.background = AVAILABLE_COLOR;
+                    timeslot.setAttribute('available', true);
                 } else {
-                    cell.style.backgroundColor = AVAILABLE_COLOR;
+                    timeslot.style.background = UNAVAILABLE_COLOR;
+                    timeslot.setAttribute('available', false);
                 }
             }
         }
     }
+
+    async function submit() {
+        let CSRFToken = document.cookie
+            .split("; ")
+            .find((element) => element.startsWith("csrftoken="))
+            .split("=")[1];
+        console.log(availability);
+        let data = {
+            'availability': availability
+        }
+        const response = await fetch(`/api/inductionclasses/set_availability/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': CSRFToken,
+            },
+            body: JSON.stringify(data)
+        });
+        if (response.ok) {
+            console.log('Availability updated');
+        } else {
+            console.log('Failed to update availability');
+        }
+    }
+
 
 </script>
 
@@ -48,7 +131,10 @@
     <div style="padding-left:50px">
         <h1>Interview Availability</h1>
     </div>
-    <div id="schedule"></div>
+    <div style="display: flex; flex-direction: row;">
+        <div id="schedule"></div>
+        <button on:click={submit}>Submit</button>
+    </div>
 </body>
 </Layout>
 
@@ -56,7 +142,8 @@
     #schedule {
         display: flex;
         flex-direction: row;
-        padding-left: 10px;
+        padding-left: 50px;
+        padding-bottom: 3vh;
         width: 80%;
         height: 100%;
     }
