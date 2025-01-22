72b08acb910f53736bec9d24a0cd8d48526e9296
Guidong Luo <g1luo@ucsd.edu>
2025-01-22 12:22:18 -0800

    feat: tailwind setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+import { setContext } from 'svelte';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,svelte}"],
+  theme: {
+    extend: {
+      colors: {
+
+        primary: "#183E51", // Dark Blue
+        secondary: "#5FBFF9", // light blue
+
+        white: '#FFFFFF', // White
+        black: '#000000', // Black
+      },
+    },
+  },
+  plugins: [],
+}
+
