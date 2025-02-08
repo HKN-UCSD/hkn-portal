fbd5feb0a00547bc9f7034436b662232831f519b
Guidong Luo <g1luo@ucsd.edu>
2025-02-07 22:59:19 -0800

    fix: restyle nav link

--- frontend/src/Components/NavLink.svelte
+++ frontend/src/Components/NavLink.svelte
@@ -3,6 +3,12 @@
     export let link;
 </script>
 
-<a href={link} on:click class ="rounded-lg p-2 mx-1 text-sm md:text-lg hover:bg-slate-100 hover:text-primary">
-    {text}
+<a
+  href={link}
+  on:click
+  class="relative px-4 py-2 mx-1 text-sm md:text-lg font-medium transition duration-300
+         text-white hover:text-white after:block after:h-0.5 after:w-full after:bg-white
+         after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100"
+>
+  {text}
 </a>
