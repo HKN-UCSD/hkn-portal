a545cbed4b82fd41b7a977f9f57a9ad4aa237cfc
ryanyychen <ryan_yychen@hotmail.com>
2025-02-26 12:39:02 -0800

    Create ProfileIconEdit.svelte

---
+++ frontend/src/Components/ProfileIconEdit.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { onMount, onDestroy } from 'svelte';
+    export let show;
+    export let profileIcon;
+
+    export let onSave = () => {};
+    export let onClose = () => {};
+    let userInfo;
+
+    onMount(async () => {
+        const handleKeydown = (event) => {
+            if (event.key === "Escape") {
+                onClose();
+            }
+        }
+        document.addEventListener("keydown", handleKeydown);
+    });
+
+    let editedProfileIcon = profileIcon;
+
+    function saveAndClose() {
+        onSave({ profileIcon: editedProfileIcon });
+        onClose();
+    };
+
+    function onCancel() {
+        editedProfileIcon = profileIcon;
+        onClose();
+    }
+
+  </script>
