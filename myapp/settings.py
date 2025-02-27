d014d8d9ce31d89d1e1d1369103f3b671dfc71d1
ryanyychen <ryan_yychen@hotmail.com>
2025-02-26 21:50:05 -0800

    Update settings to collect static files into staticfiles instead of static

--- myapp/settings.py
+++ myapp/settings.py
@@ -164,9 +164,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "myapp", "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-if not DEBUG:
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, "myapp", "static"),) 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "myapp", "static"),) 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
