9347db56bf5c2e680b4f01d707e46b6d3c0af030
ryanyychen <ryan_yychen@hotmail.com>
2025-03-03 01:11:09 -0800

    Remove console logging of events in home

--- frontend/src/Pages/Home.svelte
+++ frontend/src/Pages/Home.svelte
@@ -51,7 +51,7 @@
             embed_code: event.embed_code ? event.embed_code : eventGraphics[event.event_type],
             is_draft: event.is_draft
         }));
-        
+
         events.sort((a, b) => new Date(a.start_time) - new Date(b.start_time));
 
         events.sort((a, b) => new Date(a.start_time) - new Date(b.start_time));
