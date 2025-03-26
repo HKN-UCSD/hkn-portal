5c0ac05651dd02620acc369aa63491d00538ad51
jyeh2 <yehjames526@gmail.com>
2025-03-27 05:45:45 +0800

    Final fix for format issue 1

--- frontend/src/Components/Events/CustomizableEventConsole.svelte
+++ frontend/src/Components/Events/CustomizableEventConsole.svelte
@@ -268,8 +268,8 @@
                     Sign In List
                 </button>
                 <button class="text-white px-4 py-2 rounded"
-                    class:bg-primary={!buttonBackgroundToggle}
-                    class:bg-secondary={buttonBackgroundToggle}
+                    class:bg-primary={buttonBackgroundToggle}
+                    class:bg-secondary={!buttonBackgroundToggle}
                     on:click={() => {
                         selectedProperties = ["Name", "Email", "RSVP Time"];
                         hiddenProperties = [];
