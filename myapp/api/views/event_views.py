15c53219e20a6f56d8050252420763e99485989f
bnghiem18 <118479808+bnghiem18@users.noreply.github.com>
2025-04-16 15:27:52 -0700

    sorry ryan

--- myapp/api/views/event_views.py
+++ myapp/api/views/event_views.py
@@ -113,12 +113,12 @@ class EventActionRecordViewSet(ModelViewSet):
             if is_admin(user):
                 return qs
 
-            return qs.filter(user=self.request.user)
+            return qs.filter(acted_on=self.request.user)
         else:
             if is_admin(user):
                 return qs.filter(event__pk = eventid)
 
-            return qs.filter(user=self.request.user, event__pk = eventid)
+            return qs.filter(acted_on=self.request.user, event__pk = eventid)
 
     def destroy(self, request, *args, **kwargs):
         # TODO: remove users' ability to check themselves off
