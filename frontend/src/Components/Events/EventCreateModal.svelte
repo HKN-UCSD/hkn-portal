80e33666048dec95ae551146149a982ebd25910f
eajbros <146775668+eajbros@users.noreply.github.com>
2025-05-28 12:48:45 -0700

    Fixed my fix, form updates successfully now

--- frontend/src/Components/Events/EventCreateModal.svelte
+++ frontend/src/Components/Events/EventCreateModal.svelte
@@ -112,9 +112,10 @@
         formData.set("start_time", start_date_in_utc);
         formData.set("end_time", end_date_in_utc);
         
-        formData.set("is_draft", !formData.get("is_ready"));
-        formData.set("is_time_restricted", formData.get("time_restricted"));
-        formData.set("anon_viewable", formData.get("visible_to_guests"));
+        //Checkboxes == null when unchecked
+        formData.set("is_draft", formData.get("is_ready") == null);
+        formData.set("is_time_restricted", formData.get("time_restricted") != null);
+        formData.set("anon_viewable", formData.get("visible_to_guests") != null);
 
         selectedHosts.forEach(h => formData.append("hosts", h.user_id));
         formData.append("view_groups", 4) // Add officer view permission
