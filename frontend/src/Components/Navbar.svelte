0df1935b48aa462a739e0287df9f92f398ea8c7c
ominjoo <o.minjoo14@gmail.com>
2026-01-28 12:28:15 -0800

    fixed path logo

--- frontend/src/Components/Navbar.svelte
+++ frontend/src/Components/Navbar.svelte
@@ -5,7 +5,7 @@
   import NavLink from './NavLink.svelte';
   import ProfileIcon from './ProfileIcon.svelte';
 
-  let logo = "/static/HKN-Logo-New-Blue.png";
+  let logo = "./public/HKN-Logo-New-Blue.png";
   let isOpen = false; // Mobile menu state
 
   import { onMount } from 'svelte';
