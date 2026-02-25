7facc1fba66ac380b698a1e3f1f39635c4da1586
jyeh2 <yehjames526@gmail.com>
2026-02-25 12:58:55 -0800

    added changes to packages, need to remove svelte devices

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -35,8 +35,6 @@ export default defineConfig(({ mode }) => {
         },
 
         server: {
-            port: 3000,
-            strictPort: false,
             proxy: {
                 '/api': {
                     target: 'http://localhost:8000',
